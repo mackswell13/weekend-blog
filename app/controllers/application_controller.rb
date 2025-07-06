13363c5abcddcce9d1d8692eeade144c3deb0410
Max Sherwin <maxwellsherwin@proton.me>
2025-07-05 20:40:33 -0500

    Fix add home page buttons for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  before_action :set_is_admin
-
-  private
-
-  def set_is_admin
-    if Current.user
-      @is_admin = Current.user.is_admin
-    end
-  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
   allow_unauthenticated_access
 
   def home
+    authenticated?
     @resume_bullets = ResumeBullet.order(created_at: :desc).limit(5)
     @blogs = Blog.order(created_at: :desc).limit(5)
     @links = LinkedBlog.order(created_at: :desc).limit(5)
+    @is_admin = Current.user.is_admin
   end
 end
